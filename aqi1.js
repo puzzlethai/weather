04dde2e430370ebc7b5c84871b50b10a0120b35a
OZONE <iq_maze@yahoo.com>
2018-05-06 15:33:55 +0700

    1.3 add script bangkok before

--- aqi1.js
+++ aqi1.js
@@ -1,4 +1,12 @@
-(function(w,d,t,f){  w[f]=w[f]||function(c,k,n){s=w[f],k=s['k']=(s['k']||(k?('&k='+k):''));s['c']=
-    c=(c  instanceof  Array)?c:[c];s['n']=n=n||0;L=d.createElement(t),e=d.getElementsByTagName(t)[0];
-    L.async=1;L.src='//feed.aqicn.org/feed/'+(c[n].city)+'/'+(c[n].lang||'')+'/feed.v1.js?n='+n+k;
-    e.parentNode.insertBefore(L,e);  };  })(  window,document,'script','_aqiFeed'  );
+(function(aqi){ var x = window['_aqiFeed'], conf = x['c'],n = x['n']++, c = aqi.opt = conf[n]; x.show = function(id,jq)
+{ var o=document.getElementById(id); if (o) o.style.display='';};
+x.hide = function(id,jq) { var o=document.getElementById(id); if (o) o.style.display='none'; };
+x.adjustTooltipPosition = function (id) {try {var d = document, tooltip = d.getElementById(id), r = tooltip.getBoundingClientRect(), w = window.innerWidth || d.documentElement.clientWidth || d.body.clientWidth;if (r.left > w-210) {var dx = r.left-(w-210);var o = d.getElementById('details-'+id);o.style.marginLeft='-'+dx+'px';}}
+catch (e) {}};
+var text = function(o) {return function(t) {return t.replace(/%\w*/g,function(t){return o[t.substr(1)]||'-';}); }};
+var instance = function() {var l = x['l']||0; x['l']=l+1;
+var obj = JSON.parse(JSON.stringify(aqi).replace(/xppid-1525587275/g,'aqi-popup-'+l));
+obj.details = obj.popup.frame.replace("<!-FC->",aqi.popup.content+obj.popup.attribution);
+var saqi = obj.aqit.replace('<!-PP->',obj.popup.container.replace("<!-PC->",obj.popup.arrow+obj.details));
+obj.aqi = saqi.replace(/custom-css/,obj.style);obj.aqiv = saqi;obj.text = text(obj);return obj;};
+var obj = instance();obj.instance = instance;if (c.callback) { try { c.callback&&c.callback(obj); } catch (e) { console&&console.log(e); } }else if (c.container) { o = document.getElementById(c.container); if (o) o.innerHTML=text(obj)(c.display || '%cityname %aqi'); }if (n<conf.length-1) _aqiFeed(conf,null,n+1);}({"cityname":"Bangkok","popup":{"id":"xppid-1525587275","content":"
